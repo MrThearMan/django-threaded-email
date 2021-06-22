723f748528427382574852eee18adc83a4f750ea
Matti Lamppu <matti.lamppu@vincit.fi>
2021-06-22 20:31:16 +0300

    Simplify Thread creation.

--- django_threaded_email/django_threaded_email.py
+++ django_threaded_email/django_threaded_email.py
@@ -4,7 +4,7 @@ import logging
 from typing import Optional, List
 from threading import Thread
 
-from django.core import mail
+from django.core.mail import send_mail
 from django.core.mail.backends.base import BaseEmailBackend
 
 
@@ -14,49 +14,6 @@ __all__ = [
 
 
 logger = logging.getLogger(__name__)
-
-
-class EmailThread(Thread):
-    """Spin a simple email sending thread."""
-
-    def __init__(
-        self,
-        subject: str,
-        message: str,
-        from_email: Optional[str],
-        recipient_list: List[str],
-        fail_silently: bool = False,
-        auth_user: str = None,
-        auth_password: str = None,
-        connection: "BaseEmailBackend" = None,
-        html_message: str = None,
-    ):
-        self.subject = subject
-        self.message = message
-        self.recipient_list = recipient_list
-        self.fail_silently = fail_silently
-        self.auth_user = auth_user
-        self.auth_password = auth_password
-        self.from_email = from_email
-        self.connection = connection
-        self.html_message = html_message
-        Thread.__init__(self)
-
-    def run(self):
-        try:
-            mail.send_mail(
-                subject=self.subject,
-                message=self.message,
-                from_email=self.from_email,
-                recipient_list=self.recipient_list,
-                fail_silently=self.fail_silently,
-                auth_user=self.auth_user,
-                auth_password=self.auth_password,
-                connection=self.connection,
-                html_message=self.html_message,
-            )
-        except Exception as e:  # noqa
-            logger.error(f"Could not send email: {e}")
 
 
 def send_threaded_mail(
@@ -85,14 +42,21 @@ def send_threaded_mail(
                          Falls back to message if recipient's email client cant read HTML.
     """
 
-    EmailThread(
-        subject=subject,
-        message=message,
-        from_email=from_email,
-        recipient_list=recipient_list,
-        fail_silently=fail_silently,
-        auth_user=auth_user,
-        auth_password=auth_password,
-        connection=connection,
-        html_message=html_message,
-    ).start()
+    def task():
+        return send_mail(
+            subject=subject,
+            message=message,
+            from_email=from_email,
+            recipient_list=recipient_list,
+            fail_silently=fail_silently,
+            auth_user=auth_user,
+            auth_password=auth_password,
+            connection=connection,
+            html_message=html_message,
+        )
+
+    thread = Thread(target=task)
+    try:
+        thread.start()
+    except Exception as e:
+        logger.error(f"Could not send email: {e}")
